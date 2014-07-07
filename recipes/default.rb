268300174f6bda7f11ea37ce85bacc8b245fdccd
Stefan Trauth <mail@stefantrauth.de>
2014-07-07 20:54:46 +0200

    install correct nginx package

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,13 +15,17 @@ apt_repository 'nginx-passenger' do
 end
 
 # install nginx which contains passenger support
-package "nginx-full"
+package "nginx-extras"
+package "passenger"
 
+# define nginx service as the nginx::passenger recipe restarts the service
+# and fails if the service does not exist
 service 'nginx' do
   supports :status => true, :restart => true, :reload => true
   action   :start
 end
 
+# installs the passenger gem
 include_recipe "nginx::passenger"
 
 # create server config template in nginx config directory
