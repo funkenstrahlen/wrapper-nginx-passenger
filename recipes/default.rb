1b823fe7762ec426b2dc9d28822ced1455f974ca
Stefan Trauth <mail@stefantrauth.de>
2014-07-07 11:16:27 +0200

    overwrite template

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,3 +8,12 @@
 
 include_recipe "nginx::source"
 include_recipe "nginx::passenger"
+
+# this has to be done to overwrite the template of the nginx cookbook
+begin
+  t = resources(:template => "/etc/init/nginx.conf")
+  t.source "nginx.conf.erb"
+  t.cookbook "wrapper-nginx-passenger"
+  rescue Chef::Exceptions::ResourceNotFound
+    Chef::Log.warn "could not find template for nginx.conf.erb to modify"
+end
