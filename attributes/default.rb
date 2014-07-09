ad8cd755ff7d800ec57f524966acbdb402b45e4f
Stefan Trauth <mail@stefantrauth.de>
2014-07-09 17:21:42 +0200

    only compile nginx passenger module

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default['nginx']['passenger']['port'] = 8080
 # use override as this is a wrapper cookbook
-override['nginx']['source']['modules'] = ["nginx::passenger", "http_stub_status_module", "http_ssl_module", "http_gzip_static_module"]
+override['nginx']['source']['modules'] = ["nginx::passenger"]
